5bac45c4e99a511b78a30059a23f2c8d8a8a3658
Motty174 <sahakyanonik174@gmail.com>
2020-11-20 19:29:39 +0400

    go

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
+//Modules Connected
 const express=require('express')
-const { Socket } = require('socket.io')
 const app=express()
-const server=require('http').createServer(app)
-const io=require('socket.io')(server)
+const PORT=process.env.PORT || 8080
+
+// Creating server
+const server=app.listen(PORT,(err,msg)=>{
+    if(err)throw err;
+    console.log(`[Server running on PORT:${PORT}]`)
+})
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html')
+    res.sendFile(__dirname+"/chat.html")
 })
 
-io.on('connection',(socket)=>{
-    socket.on('chat message', (msg) => {
-        io.emit('chat message','User: '+msg)
-      });   
-       socket.on('disconnect', () => {
-        console.log('user disconnected');
-      });
-})
+//Socket io part starts from here
+const io=require('socket.io')(server)
 
 
-const PORT=process.env.PORT || 8080
-server.listen(PORT,()=>{
-    console.log('Connected on port: '+PORT)
+const users=[]
+const connections=[]
+
+io.on('connection',(socket,name)=>{
+    socket.on('new user',(name)=>{
+        users.push(name)
+        socket.broadcast.emit('broadcast', users[users.length-1]+' :joined to chat');
+    })
+    
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('broadcast', 'Disconnected from chat');
+    })
+    socket.on('chat msg',(msg,name)=>{
+        io.emit('chat msg',msg,name)
+    })
+
 })
+
